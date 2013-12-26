09d006137341188c8f3eb9394f1ac622011f74d9
raylu <raylu@gridium.com>
2013-12-26 14:27:07 -0800

    use lxml, extract (some) formatting

--- binder.py
+++ binder.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from epub import ez_epub
+import lxml.html
 
 import os
 import re
@@ -20,33 +21,53 @@ def main():
 
 	for filename in files:
 		print 'binding', filename
-		with open('html/%s' % filename) as f:
-			html = f.readlines()
 		section = ez_epub.Section()
 		section.title = filename
-		state = None
-		for line in html:
-			line = line.strip().decode('utf-8')
-			if '<div class="date">' in line:
-				state = 'date'
-			elif '<div class="entry">' in line:
-				state = 'entry'
-			elif '<hr>' in line:
-				state = None
-			line = re.sub('&#\d{4};', repl_entity, line)
-			if state is not None:
-				while True:
-					line, subs = re.subn('<.*?>', '', line)
-					if subs == 0:
-						break
-				section.text.append(line)
+		section.css = '''
+			.h2 { font-size: 24pt; font-weight: bold; }
+			.em { font-style: italic; }
+			.strong { font-weight: bold; }
+			.em_strong { font-style: italic; font-weight: bold; }
+		'''
+
+		dom = lxml.html.parse('html/%s' % filename)
+		for div in dom.getroot().iterdescendants('div'):
+			div_class = div.get('class')
+			if div_class not in ['date', 'entry']:
+				continue
+			last_segments = extract_text(section.text, div, [])
+			if last_segments:
+				section.text.append(last_segments)
 		book.sections.append(section)
+		break
 
 	book.make('tomu_vol1')
 
-def repl_entity(match):
-	entity = match.group(0)
-	return unichr(int(entity[2:6]))
+def extract_text(text, el, styles):
+	segments = []
+	if el.text:
+		segments.append((el.text, '_'.join(sorted(styles))))
+	for sub_el in el:
+		style = None
+		if sub_el.tag == 'hr': # end of the chapter
+			break
+		elif sub_el.tag in ['p', 'br']:
+			text.append(segments)
+			segments = []
+		elif sub_el.tag in ['h2', 'em', 'b', 'strong']:
+			if sub_el.tag == 'b':
+				sub_el.tag = 'strong'
+			style = sub_el.tag
+		elif sub_el.tag not in ['a', 'span', 'center']:
+			print 'unhandled tag', sub_el.tag
+		if style is not None:
+			styles.append(style)
+		segments.extend(extract_text(text, sub_el, styles))
+		if style is not None:
+			styles.pop()
+	if el.tail:
+		segments.append((el.tail, '_'.join(sorted(styles))))
+	return segments
 
 if __name__ == '__main__':
 	main()
