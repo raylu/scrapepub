52b6c5e6031321566e095c103e39b8d17df6ad28
raylu <raylu@gridium.com>
2013-12-26 19:31:23 -0800

    no more ez_epub; stick the entire html fragment in

--- binder.py
+++ binder.py
@@ -1,29 +1,26 @@
 #!/usr/bin/env python
 
-from epub import ez_epub
+from epub import epub
+import lxml.etree
 import lxml.html
 
 import os
-import re
-
-css = {
-	'h2': 'font-weight: bold;',
-	'em': 'font-style: italic;',
-	'strong': 'font-weight: bold;',
-	'center': '{text-align: center;',
-	'em_strong': 'font-style: italic; font-weight: bold;',
-	'center_strong': 'text-align: center; font-weight: bold;',
-	'center_em_strong': 'text-align: center; font-style: italic; font-weight: bold;',
-}
+import shutil
 
 def main():
-	book = ez_epub.Book()
-	book.title = 'Tales of MU - Volume 1'
-	book.authors = ['Alexandra Erin']
+	book = epub.EpubBook()
+	book.setTitle('Tales of MU - Volume 1')
+	book.addCreator('Alexandra Erin')
+	book.addTitlePage()
+	book.addTocPage()
 
-	stylesheet = ''
-	for class_name, styles in css.items():
-		stylesheet += '.%s {%s}' % (class_name, styles)
+	template = '''
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<head><title>%s</title></head>
+	<body><p>%s</p></body>
+	</html>
+	'''
 
 	files = []
 	for filename in os.listdir('html'):
@@ -35,56 +32,28 @@ def main():
 
 	for filename in files:
 		print 'binding', filename
-		section = ez_epub.Section()
-		section.title = filename
-		section.css = stylesheet
+		with open('html/%s' % filename, 'r') as f:
+			html = f.read()
+		html = html.replace('\r', '')
+		dom = lxml.html.document_fromstring(html)
+		for div in dom.iterdescendants('div'):
+			if div.get('class') == 'post':
+				break
+		else:
+			raise RuntimeError('could not find div with class="post"')
 
-		dom = lxml.html.parse('html/%s' % filename)
-		for div in dom.getroot().iterdescendants('div'):
-			div_class = div.get('class')
-			if div_class not in ['date', 'entry']:
-				continue
-			last_segments = extract_text(section.text, div, [])
-			if last_segments:
-				section.text.append(last_segments)
-		book.sections.append(section)
+		content = ''
+		for el in div.iterchildren():
+			content += lxml.etree.tostring(el)
 
-	book.make('tomu_vol1')
+		n = book.addHtml('', '%s.html' % filename, template % (filename, content))
+		book.addSpineItem(n)
+		book.addTocMapNode(n.destPath, filename)
 
-def extract_text(text, el, styles):
-	segments = []
-	if el.text:
-		segments.append(format_tuple(el.text, styles))
-	for sub_el in el:
-		style = None
-		if sub_el.tag == 'hr': # end of the chapter
-			break
-		elif sub_el.tag in ['p', 'br']:
-			text.append(segments)
-			segments = []
-		elif sub_el.tag in ['h2', 'em', 'b', 'strong', 'center', 'strike']:
-			if sub_el.tag == 'b':
-				sub_el.tag = 'strong'
-			style = sub_el.tag
-		elif sub_el.tag not in ['a', 'span', 'wbr']:
-			raise RuntimeError('unhandled tag ' + sub_el.tag)
-		if style is not None:
-			styles.append(style)
-		segments.extend(extract_text(text, sub_el, styles))
-		if style is not None:
-			styles.pop()
-		if sub_el.tag == 'h2':
-			text.append(segments)
-			segments = []
-	if el.tail:
-		segments.append(format_tuple(el.tail, styles))
-	return segments
-
-def format_tuple(text, styles):
-	style = '_'.join(sorted(styles))
-	if style and style not in css.iterkeys():
-		raise RuntimeError('unhandled style: ' + style)
-	return (text, style)
+	output_name = 'tomu_vol1'
+	shutil.rmtree(output_name, ignore_errors=True)
+	book.createBook(output_name)
+	epub.EpubBook.createArchive(output_name, output_name + '.epub')
 
 if __name__ == '__main__':
 	main()
