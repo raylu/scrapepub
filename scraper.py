932720604e13b2e0b6c1d5f72868badd7aef9a77
raylu <lurayl@gmail.com>
2014-11-08 02:20:10 -0800

    fix some bad links

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import os.path
 import re
 
 import requests
@@ -21,7 +22,14 @@ for line in split:
 		started = True
 
 for i, url in enumerate(urls):
+	path = 'raw/%d' % i
+	if os.path.exists(path):
+		continue
+	if i == 65 and url == 'https://pactwebserial.wordpress.com/2014/07/08/693/':
+		url = 'https://pactwebserial.wordpress.com/2014/07/08/signature-8-1/'
 	print 'getting', i, url
+	if url.startswith('pactwebserial.wordpress.com'):
+		url = 'https://' + url
 	html = rs.get(url).content
-	with open('raw/%d' % i, 'w') as f:
+	with open(path, 'w') as f:
 		f.write(html)
