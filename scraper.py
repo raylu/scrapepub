f0269feae792b067f60ec7047fff554cb52a2b34
raylu <lurayl@gmail.com>
2016-10-15 21:29:17 -0700

    book2 scraper

--- scraper.py
+++ scraper.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
 import requests
 from bs4 import BeautifulSoup
 
-start = 'https://practicalguidetoevil.wordpress.com/2015/03/25/prologue/'
-end = 'https://practicalguidetoevil.wordpress.com/2015/11/04/prologue-2/'
+book = sys.argv[1]
+bounds = {
+	'book1': (
+		'https://practicalguidetoevil.wordpress.com/2015/03/25/prologue/',
+		'https://practicalguidetoevil.wordpress.com/2015/11/04/prologue-2/'
+	),
+	'book2': (
+		'https://practicalguidetoevil.wordpress.com/2015/11/04/prologue-2/',
+		'https://practicalguidetoevil.wordpress.com/2016/10/12/heroic-interlude-prise-au-fer/'
+	),
+}
+start, end = bounds[book]
+dirname = 'pgte_%s_raw/' % book
+os.mkdir(dirname)
 
 rs = requests.Session()
 url = start
@@ -14,7 +29,7 @@ while url != end:
 	filename = '%02d-%s' % (i, name)
 	print 'getting', filename
 	content = rs.get(url).content
-	with open('raw/' + filename, 'w') as f:
+	with open(dirname + filename, 'w') as f:
 		f.write(content)
 	soup = BeautifulSoup(content, 'lxml')
 	next_el = soup.find('link', rel='next')
