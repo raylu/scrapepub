b7f40340e36f408a5569866dbc2d323d4a518f46
raylu <lurayl@gmail.com>
2018-09-16 23:55:22 -0700

    fix the binder

--- binder.py
+++ binder.py
@@ -22,23 +22,28 @@ def main():
 	</html>
 	'''
 
-	files = os.listdir('raw')
-	files.sort(key=int)
+	files = os.listdir('pact_raw')
+	files.sort(key=lambda n: int(n[:3]))
 
 	for filename in files:
 		print 'binding', filename
-		with open('raw/' + filename, 'r') as f:
+		with open('pact_raw/' + filename, 'r') as f:
 			html = f.read()
 		html = html.replace('\r', '')
 		dom = lxml.html.document_fromstring(html)
 
 		h1s = list(dom.iterdescendants('h1'))
-		if len(h1s) < 2:
-			raise RuntimeError('expected at least 2 h1')
-		title = h1s[-1].text
+		if filename == '150-pact-sealed':
+			title = h1s[0].text
+		else:
+			if len(h1s) < 2:
+				raise RuntimeError('expected at least 2 h1')
+			title = h1s[1].text
 
 		for div in dom.iterdescendants('div'):
 			if div.get('class') == 'entry-content':
+				break
+			if filename == '150-pact-sealed' and div.get('class') == 'entry clear':
 				break
 		else:
 			raise RuntimeError('could not find class="entry-content"')
