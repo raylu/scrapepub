b540dfcf0be3021ae7c6050678a667d0e01c98c5
raylu <lurayl@gmail.com>
2018-09-16 23:46:19 -0700

    fix all sorts of things about the scraper

--- scraper.py
+++ scraper.py
@@ -20,16 +20,26 @@ for line in split:
 			urls.append(match.group(1))
 	elif 'class="entry-content"' in line:
 		started = True
+urls.extend([
+	'https://pactwebserial.wordpress.com/2015/03/03/judgment-16-13/',
+	'https://pactwebserial.wordpress.com/2015/03/07/epilogue/',
+	'https://wildbow.wordpress.com/2015/03/07/pact-sealed/',
+])
 
 for i, url in enumerate(urls):
-	path = 'raw/%d' % i
-	if os.path.exists(path):
+	if i == 52 and url == 'https://pactwebserial.wordpress.com/table-of-contents/':
 		continue
-	if i == 65 and url == 'https://pactwebserial.wordpress.com/2014/07/08/693/':
-		url = 'https://pactwebserial.wordpress.com/2014/07/08/signature-8-1/'
-	print 'getting', i, url
+	elif i == 130 and url == '4031384495743822299':
+		url = 'https://pactwebserial.wordpress.com/2015/01/08/possession-15-2/'
+	ch = url.rsplit('/', 2)[-2]
+	name = '%03d-%s' % (i, ch)
+	path = 'pact_raw/' + name
+	if os.path.exists(path):
+		print 'already have', name
+		continue
 	if url.startswith('pactwebserial.wordpress.com'):
 		url = 'https://' + url
+	print 'getting', name
 	html = rs.get(url).content
 	with open(path, 'w') as f:
 		f.write(html)
