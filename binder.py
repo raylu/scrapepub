9e6c2a077b796754600951ae65090d5d293bab49
raylu <lurayl@gmail.com>
2019-05-01 22:44:32 -0700

    handle paste-embed-wrapper and password-protected chapters

--- binder.py
+++ binder.py
@@ -36,8 +36,6 @@ def main(vol):
 	files = os.listdir(dirname)
 	files.sort()
 
-	chapter_link_text = ['Previous Chapter', 'Next Chapter']
-
 	for filename in files:
 		print 'binding', filename
 		with open(dirname + filename, 'r') as f:
@@ -45,35 +43,46 @@ def main(vol):
 		title = soup.find(class_='entry-title').string
 		content = soup.find(class_='entry-content')
 
-		if vol < 'vol4':
-			content.find(class_='wpcnt').decompose()
-
-		# remove prev/next chapter links and random ad stylesheet
-		p_removed = 0
-		for el in content.children:
-			if el.name == 'style':
-				el.decompose()
-			elif el.name == 'p':
-				has_chapter_links = has_nontext = False
-				for c in el.children:
-					if (c.name == 'a' and c.string in chapter_link_text) or (c.name == 'span' and \
-							any(cc.name == 'a' and cc.string in chapter_link_text for cc in c.children)):
-						el.decompose()
-						p_removed += 1
-						break
-					elif c.name == 'span' and c.attrs['style'] == 'color:#8ae8ff;':
-						c.attrs['style'] = 'color:#444477;'
-		if p_removed not in (1, 2):
-			raise Exception('removed %d' % p_removed)
-
-		n = book.addHtml('', '%s.html' % filename, template % (title, title, content))
-		book.addSpineItem(n)
-		book.addTocMapNode(n.destPath, title)
+		if process_chapter(vol, content):
+			n = book.addHtml('', '%s.html' % filename, template % (title, title, content))
+			book.addSpineItem(n)
+			book.addTocMapNode(n.destPath, title)
 
 	output_name = 'inn_' + vol
 	shutil.rmtree(output_name, ignore_errors=True)
 	book.createBook(output_name)
 	epub.EpubBook.createArchive(output_name, output_name + '.epub')
+
+chapter_link_text = ['Previous Chapter', 'Next Chapter']
+
+def process_chapter(vol, content):
+	if vol < 'vol4':
+		content.find(class_='wpcnt').decompose()
+
+	# remove prev/next chapter links and random ad stylesheet
+	p_removed = 0
+	for el in content.children:
+		if el.name == 'form' and el.attrs['class'] == ['post-password-form']:
+			# password-protected chapter
+			return False
+		elif el.name == 'style':
+			el.decompose()
+		elif el.name == 'p':
+			has_chapter_links = has_nontext = False
+			for c in el.children:
+				if (c.name == 'a' and c.string in chapter_link_text) or (c.name == 'span' and \
+						any(cc.name == 'a' and cc.string in chapter_link_text for cc in c.children)):
+					el.decompose()
+					p_removed += 1
+					break
+				elif c.name == 'span' and c.attrs['style'] == 'color:#8ae8ff;':
+					c.attrs['style'] = 'color:#444477;'
+		elif el.name == 'div' and el.attrs['id'] == 'paste-embed-wrapper':
+			el.decompose()
+			p_removed += 1
+	if p_removed not in (1, 2):
+		raise Exception('removed %d' % p_removed)
+	return True
 
 if __name__ == '__main__':
 	main(*sys.argv[1:])
